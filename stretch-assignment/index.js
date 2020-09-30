740324d9598e12b2e1f6db61721fbe7112b5f6ec
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-09-29 20:49:31 -0400

    still working stretch

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -9,16 +9,40 @@ function moveToTop(e) {
     blocks.prepend(e.target)
 }
 
-const blockRed = document.querySelector(".block--red")
-blockRed.addEventListener("mousedown", movingBlock)
-function movingBlock(e) {
-    let count = 0
-    const toRight = setInterval(function() {
-        if (count === 100000000) {
-            clearInterval()
-        }
-        count += 10
-        return count
-    }, 10)
-    e.target.style.margin = `0 0 0 ${toRight}px`
+const redBlock = document.querySelector(".block--red")
+redBlock.addEventListener("mousedown mouseup", moveRight)
+
+function moveRight(e) {
+    while (e.type === "mousedown") {
+        let count = 0
+        const toRight = setInterval(function() {
+            if (count === 100000000) {
+            }
+            count += 10
+            return count
+        }, 10)
+        e.target.style.margin = `0 0 0 ${toRight}px`
+    }
 }
+
+
+// const blockRed = document.querySelector(".block--red")
+// blockRed.addEventListener("mousedown", movingBlock)
+// blockRed.addEventListener("mouseup", stopMove)
+// function movingBlock(e) {
+//     if (e.type === "mousedown") {
+//         e.preventDefault()
+//         let count = 0
+//         const toRight = setInterval(function() {
+//             if (count === 100000000) {
+//             }
+//             count += 10
+//             return count
+//         }, 10)
+//         e.target.style.margin = `0 0 0 ${toRight}px`
+//     }
+// }
+
+// function stopMove(e) {
+//     e.clearInterval()
+// }
